ab588d4104646a14959bd427fd676b489aa1e863
raretech-hal <halraretech@gmail.com>
2026-03-18 11:26:15 +0900

    todoモデル、migration作成

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TodoTask(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
