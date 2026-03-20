f4835dbeeadb5ffd55ecfbe3af27bb737664c7da
raretech-hal <halraretech@gmail.com>
2026-03-20 14:14:46 +0900

    todo一覧画面をハードコーディング、ルーティング設定など

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from .models import TodoTask
+#from .forms import TodoTaskForm
 
-# Create your views here.
+# Todoリスト一覧
+def todo_list(request):
+    tasks = TodoTask.objects.all()
+    return render(request, 'todo/todo_list.html', {'tasks': tasks})
+
