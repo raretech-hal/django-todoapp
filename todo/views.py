c0de1deccf38798569a78426cace602ec86bad6a
raretech-hal <halraretech@gmail.com>
2026-03-21 00:56:41 +0900

    更新処理の実装

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import TodoTask
 from .forms import TodoTaskForm
 
 # Todoリスト一覧、作成
 def todo_list(request):
     if request.method == 'POST':
-        form = TodoTaskForm(request.POST)
+        task_id = request.POST.get('task_id')
+        if task_id: #更新処理時
+            task = get_object_or_404(TodoTask, id=task_id)
+            form = TodoTaskForm(request.POST, instance=task)
+        else: #新規作成時
+            form = TodoTaskForm(request.POST)
+
         if form.is_valid():
             form.save()
             return redirect('todo_list')
