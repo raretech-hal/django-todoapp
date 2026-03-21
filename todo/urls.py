af16c6d1366fb99e2d98e620c032b25432915230
raretech-hal <halraretech@gmail.com>
2026-03-21 15:50:15 +0900

    パスワード再設定機能

--- todo/urls.py
+++ todo/urls.py
@@ -8,10 +8,15 @@ urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('delete/<int:task_id>/', views.todo_delete, name='todo_delete'),
     path('toggle/<int:task_id>/', views.toggle_completed, name='toggle_completed'),
+    ## 認証周り
     path('login/', auth_views.LoginView.as_view(
         template_name='registration/login.html'
     ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
