3857ace355bb5d77b824ab69a5dd2443dcb60e79
raretech-hal <halraretech@gmail.com>
2026-03-22 13:47:15 +0900

    モデルをAdmi画面に登録

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TodoTask, Category, Profile
 
 # Register your models here.
+admin.site.register(TodoTask)
+admin.site.register(Category)
+admin.site.register(Profile)
